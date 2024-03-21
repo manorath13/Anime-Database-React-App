2520c4f412c731e7cb3ea02464ee1c1227d51bd8
Manorath Reddy <kmanorathreddy13@gmail.com>
2024-03-21 22:48:02 +0530

    first commit

---
+++ src/Components/Airing.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { useGlobalContext } from '../context/global'
+import styled from 'styled-components'
+import Sidebar from './Sidebar'
+
+function Airing({rendered}) {
+    const {airingAnime ,isSearch, searchResults} = useGlobalContext()
+
+    const conditionalRender = () => {
+        if(!isSearch && rendered === 'airing'){
+            return airingAnime?.map((anime) => {
+                // console.log(anime);
+                return <Link to={`/anime/${anime.mal_id}`} key={anime.mal_id}>
+                    <img src={anime.images.jpg.large_image_url} alt="" />
+                    <h4>
+                    {anime.title_english === null ? anime.title  :  anime.title_english.length < 45   ? anime.title_english : anime.title_english.substr(0, 45) + '...'}
+                    </h4>
+                </Link>
+            })
+        }else{
+            return searchResults?.map((anime) => {
+                return <Link to={`/anime/${anime.mal_id}`} key={anime.mal_id}>
+                    <img src={anime.images.jpg.large_image_url} alt="" />
+                    <h4>
+                    {anime.title_english === null ? anime.title  :  anime.title_english.length < 45   ? anime.title_english : anime.title_english.substr(0, 45) + '...'}
+                    </h4>
+                </Link>
+            })
+        }
+    }
+
+    return (
+        <PopularStyled>
+            <div className="airing-anime">
+                {conditionalRender()}
+            </div>
+            <Sidebar />
+        </PopularStyled>
+    )
+}
+
+const PopularStyled = styled.div`
+    display: flex;
+    .airing-anime{
+        margin-top: 2rem;
+        padding-top: 2rem;
+        padding-bottom: 2rem;
+        padding-left: 5rem;
+        padding-right: 0;
+        width: 100%;
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+        grid-gap: 4rem;
+        background-color: #fff;
+        border-top: 5px solid #e5e7eb;
+        a{
+            height: 350px;
+            border-radius: 7px;
+            border: 5px solid #e5e7eb;
+        }
+        a img{
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+            border-radius: 5px;
+        }
+    }
+`;
+
+export default Airing
